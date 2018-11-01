c09f5828dcd0f9f8b6d0116364b6b52d963abd29
MMR <mmrrahman508@gmail.com>
2018-11-01 08:06:14 +0600

    Migration ADDED

--- Blog/urls.py
+++ Blog/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^about/$', views.about),
     url(r'^articles/', include('articles.urls')),
+    url(r'^accounts/', include('accounts.urls')),
     url(r'^$', views.home_page)
 ]
